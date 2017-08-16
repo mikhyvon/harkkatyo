64026ba8be775d2c02cdb05d455e1fd94efc1566
Mikko Hyvönen <mikkohy@solita.fi>
2017-08-16 13:45:40 +0300

    initial commit

---
+++ src/main/java/fi/solita/harkka/mikkohyv/MikkohyvApplication.java
@@ -0,0 +1,12 @@
+package fi.solita.harkka.mikkohyv;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MikkohyvApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MikkohyvApplication.class, args);
+	}
+}
