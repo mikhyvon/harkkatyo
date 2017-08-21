08dab958d16a0b64059864e52dca98808cf619f3
Marko Lauronen <Marko.Lauronen@solita.fi>
2017-08-21 09:27:55 +0300

    test-profiili käyttämään in-memory hsqldb:tä

--- src/test/java/fi/solita/harkka/mikkohyv/domain/model/TopicTests.java
+++ src/test/java/fi/solita/harkka/mikkohyv/domain/model/TopicTests.java
@@ -4,6 +4,7 @@ import fi.solita.harkka.mikkohyv.domain.shared.GenericMockRepository;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit4.SpringRunner;
 
@@ -12,7 +13,8 @@ import static org.junit.Assert.assertNotNull;
 
 @RunWith(SpringRunner.class)
 @ContextConfiguration(classes = {fi.solita.harkka.mikkohyv.application.MikkohyvApplication.class})
-@SpringBootTest()
+@SpringBootTest
+@ActiveProfiles("test")
 public class TopicTests {
 
 
